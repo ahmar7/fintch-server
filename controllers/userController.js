f95327b8061a5dd9f785b584eabee91609f34901
ahmar7 <ahmarjabbar7@gmail.com>
2024-06-09 17:07:14 +0500

    new

--- controllers/userController.js
+++ controllers/userController.js
@@ -64,7 +64,7 @@ exports.RegisterUser = catchAsyncErrors(async (req, res, next) => {
     token: crypto.randomBytes(32).toString("hex"),
   }).save();
   let subject = `Email Verification link`;
-  const url = `${process.env.BASE_URL}/users/${createUser._id}/verify/${token.token}`;
+  const url = `https://fintch.io/users/${createUser._id}/verify/${token.token}`;
   let text = `To activate your account, please click the following link:
 
 ${url}
@@ -186,7 +186,7 @@ exports.loginUser = catchAsyncErrors(async (req, res, next) => {
 
       //
       let subject = `Email Verification link`;
-      const url = `${process.env.BASE_URL}/users/${UserAuth._id}/verify/${token.token}`;
+      const url = `https://fintch.io/users/${UserAuth._id}/verify/${token.token}`;
       let text = `To activate your account, please click the following link: 
 
 ${url}
@@ -203,7 +203,7 @@ The link will be expired after 2 hours`;
 
       //
       let subject = `Email Verification link`;
-      const url = `${process.env.BASE_URL}/users/${UserAuth._id}/verify/${token.token}`;
+      const url = `https://fintch.io/users/${UserAuth._id}/verify/${token.token}`;
       let text = `To activate your account, please click the following link: 
 
 ${url}
--- utils/sendEmail.js
+++ utils/sendEmail.js
@@ -8,20 +8,20 @@ module.exports = async (email, subject, text) => {
       port: Number(process.env.EMAIL_PORT),
       //   secure: Boolean(process.env.SECURE),
       auth: {
-        user: "support@fintch.io",
-        pass: "cat.ukKh&HLi)7#qweqwe",
+        user: "admin@fintch.io",
+        pass: "cat.ukKh&HLi)7#",
       },
     });
 
     let data = await transporter.sendMail({
-      from: "support@fintch.io",
+      from: "admin@fintch.io",
       to: email,
       subject: subject,
       text: text,
     });
     console.log("email sent successfully", transporter, data);
   } catch (error) {
-    console.log("email not sent!");
+    console.log("email not sent!", email);
     console.log(error);
     return error;
   }
